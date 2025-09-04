64fbec347394868cb845c02e89b624c084c7a0ee
Brian Xiong <zyxiong@gmail.com>
2025-09-04 10:18:14 -0700

    Adding Toolchain for Buildroot

---
+++ Toolchain/vtcs_toolchain/leipzig/lib/gcc/aarch64-buildroot-linux-gnu/11.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/opt/vtcs_toolchain/leipzig/ --sysconfdir=/opt/vtcs_toolchain/leipzig//etc --enable-static --target=aarch64-buildroot-linux-gnu --with-sysroot=/opt/vtcs_toolchain/leipzig//aarch64-buildroot-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --enable-plugins --enable-lto --with-gmp=/opt/vtcs_toolchain/leipzig/ --with-mpc=/opt/vtcs_toolchain/leipzig/ --with-mpfr=/opt/vtcs_toolchain/leipzig/ --with-pkgversion='Buildroot Leipzig-SDK-v1.0-rc1' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=lp64 --with-cpu=cortex-a55 --enable-languages=c,c++ --with-build-time-tools=/opt/vtcs_toolchain/leipzig//aarch64-buildroot-linux-gnu/bin --enable-shared --enable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a55" } };
